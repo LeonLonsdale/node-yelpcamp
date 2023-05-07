c28fb004a11fc8659fb5ef9fdf7ba7d38176c70d
LeonLonsdale <leon.dev@icloud.com>
2023-05-07 22:33:09 +0100

    create basic campground index view and route

--- app.js
+++ app.js
@@ -27,11 +27,9 @@ app.set('views', path.join(__dirname, 'views'));
 // ### [ Routes ]
 
 app.get('/', (req, res) => res.render('home.ejs'));
-app.get('/test-campground', async (req, res) => {
-  const camp = new Campground({
-    title: 'Some rando campground',
-    description: `not even real, don't pay`,
-  });
-  await camp.save();
-  res.send(camp);
+
+app.get('/campgrounds', async (req, res) => {
+  const campgrounds = await Campground.find({});
+
+  res.status(200).render('campgrounds/index', {campgrounds});
 });
