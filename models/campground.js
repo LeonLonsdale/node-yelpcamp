c84228a14cce6b8940555e3fe4568cabede79890
LeonLonsdale <leon.dev@icloud.com>
2023-05-09 21:07:09 +0100

    add images and prices to seeding

--- models/campground.js
+++ models/campground.js
@@ -2,9 +2,10 @@ import mongoose, {Schema, model} from 'mongoose';
 
 const campgroundSchema = new Schema({
   title: String,
-  price: String,
+  price: Number,
   description: String,
   location: String,
+  image: String,
 });
 
 export const Campground = model('Campground', campgroundSchema);
--- seeds/index.js
+++ seeds/index.js
@@ -16,9 +16,14 @@ const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i <= 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
+      image: 'https://source.unsplash.com/collection/483251',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eget dolor morbi non arcu risus quis varius quam quisque. Facilisis leo vel fringilla est ullamcorper eget nulla. Enim nulla aliquet porttitor lacus luctus.',
+      price,
     });
     await camp.save();
   }
