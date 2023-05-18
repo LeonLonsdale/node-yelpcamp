3fc3b15b21f836a233698bb51abaa8499306be70
LeonLonsdale <leon.dev@icloud.com>
2023-05-18 22:33:43 +0100

    update seeds file to include gemetry

--- seeds/index.js
+++ seeds/index.js
@@ -19,6 +19,13 @@ const seedDB = async () => {
     const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      geometry: {
+        type: 'Point',
+        coordinates: [
+          cities[random1000].longitude,
+          cities[random1000].latitude,
+        ],
+      },
       title: `${sample(descriptors)} ${sample(places)}`,
       author: '6460fbb1a13a782a30ee4d72',
       description:
