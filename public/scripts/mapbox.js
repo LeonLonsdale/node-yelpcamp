752e681ece94921b4abffcee354ed46a21dd2052
LeonLonsdale <leon.dev@icloud.com>
2023-05-18 22:03:10 +0100

    use geodata to centre map at campground location

--- public/scripts/mapbox.js
+++ public/scripts/mapbox.js
@@ -1,7 +1,14 @@
+const coords = document
+  .getElementById('map')
+  .getAttribute('data-coords')
+  .split(',')
+  .map((num) => Number(num));
+
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
   container: 'map', // container ID
   style: 'mapbox://styles/mapbox/streets-v12', // style URL
-  center: [-74.5, 40], // starting position [lng, lat]
+  center: coords, // starting position [lng, lat]
   zoom: 9, // starting zoom
 });
+const marker = new mapboxgl.Marker().setLngLat(coords).addTo(map);
