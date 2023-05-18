92962aa3a01263ea609dcd9008dbcf8c9e913e3f
LeonLonsdale <leon.dev@icloud.com>
2023-05-18 22:46:33 +0100

    add popup to map, change map style

--- public/scripts/mapbox.js
+++ public/scripts/mapbox.js
@@ -4,11 +4,24 @@ const coords = document
   .split(',')
   .map((num) => Number(num));
 
+const campgroundTitle =
+  document.querySelector('[data-label-title]').textContent;
+const campgroundLocation = document.querySelector(
+  '[data-label-location]'
+).textContent;
+
+const popup = new mapboxgl.Popup({
+  offset: 30,
+}).setHTML(`<h5>${campgroundTitle}</h5><p>${campgroundLocation}</p>`);
+
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
   container: 'map', // container ID
-  style: 'mapbox://styles/mapbox/streets-v12', // style URL
+  style: 'mapbox://styles/mapbox/light-v10', // style URL
   center: coords, // starting position [lng, lat]
   zoom: 9, // starting zoom
 });
-const marker = new mapboxgl.Marker().setLngLat(coords).addTo(map);
+const marker = new mapboxgl.Marker()
+  .setLngLat(coords)
+  .setPopup(popup)
+  .addTo(map);
