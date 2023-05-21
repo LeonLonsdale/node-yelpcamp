9f8f2ef65b69067a32f250231b691d54df20b861
LeonLonsdale <leon.dev@icloud.com>
2023-05-21 15:01:35 +0100

    attemp to fix deployment issue

--- app.js
+++ app.js
@@ -144,6 +144,7 @@ app.use(flash());
 app.use((req, res, next) => {
   res.locals.success = req.flash('success');
   res.locals.error = req.flash('error');
+  console.log(req.user);
   res.locals.user = req.user;
   next();
 });
